2c6ade1aa939b6c1561023acc18f86a78a389e11
Bradley Fosu <P268fm@hotmail.com>
2025-12-09 19:09:42 -0500

    Update tonejs.js

--- js/tonejs.js
+++ js/tonejs.js
@@ -5,14 +5,12 @@ const button = document.querySelector(".tone");
 button.addEventListener("click", async () => {
   console.log("▶ Button clicked");
 
-  // Required for Chrome
   await Tone.start();
   console.log("🔊 Audio context started");
 
-  // Create the player ONLY after user gesture
   const player = new Tone.Player({
     url: "./audio/generique-batman-1990x27s-296162.mp3",
-    autostart: true,   // automatically plays when loaded
+    autostart: true,
   }).toDestination();
 
   await player.load();
